533463523572132adc16811321129b58b343395f
monalimodi <qamonali@gmail.com>
2026-05-29 03:03:39 -0500

    target_cart_empty.feature

--- features/environment.py
+++ features/environment.py
@@ -2,7 +2,6 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
-
 def browser_init(context):
     """
     :param context: Behave context
